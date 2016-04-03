09ff471c2c1f4323fd0c46eb8097c777902382d0
Ian Roberts <ianroberts131@gmail.com>
2016-04-03 01:54:47 +0000

    Checkpoint 31 CRUD Intro: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :posts
+
   get 'welcome/index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
   
   root 'welcome#index'
   
-  get 'welcome/contact'
 
 end
