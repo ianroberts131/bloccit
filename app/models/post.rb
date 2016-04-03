e5d583667a5ac0a8cdb325b8e896223ea64d9c48
Ian Roberts <ianroberts131@gmail.com>
2016-04-03 16:31:18 +0000

    Assignment 31 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  
+  Post.all.each do |post|
+    if (post.id == 1) || (post.id % 5 == 0)
+      post.title.replace "SPAM"
+      post.save!
+    end
+  end
 end
