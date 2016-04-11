5d0117fa7a239543ad7c6bd13a308796fe937d33
Ian Roberts <ianroberts131@gmail.com>
2016-04-11 12:46:50 +0000

    Fix Assignment 36

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  before_save { self.name = name.split(' ').map { |name| name.capitalize }.join(' ') }
+  before_save do
+    if name
+      self.name = name.split(' ').map { |name| name.capitalize }.join(' ')
+    end
+  end
   
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
