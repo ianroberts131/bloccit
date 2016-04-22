ba4e863bed68678dd9499bc9641a4bc6fb9390fe
Ian Roberts <ianroberts131@gmail.com>
2016-04-22 03:52:11 +0000

    Updated Checkpoint 44 - forgot to save some files

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development? || Rails.env.production?
     address:        'smtp.sendgrid.net',
     port:           '2525',
     authentication: :plain,
-    user_name:      ENV[''],
-    password:       ENV[''],
+    user_name:      ENV['app48892449@heroku.com'],
+    password:       ENV['fajepwtk3814'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
