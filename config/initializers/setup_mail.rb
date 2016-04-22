76591c6441251c39c7923fe45d7a8ce43f41335a
Ian Roberts <ianroberts131@gmail.com>
2016-04-22 14:08:04 +0000

    Fixed setup_mail initializer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development? || Rails.env.production?
     address:        'smtp.sendgrid.net',
     port:           '2525',
     authentication: :plain,
-    user_name:      ENV['app48892449@heroku.com'],
-    password:       ENV['fajepwtk3814'],
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
