60535428d0045fcf962fcfa64d0f971e9435bb32
Ian Roberts <ianroberts131@gmail.com>
2016-04-23 22:30:58 +0000

    Checkpoint 46 Private Topics: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,11 +4,16 @@ class TopicsController < ApplicationController
   before_action :authorize_user, except: [:index, :show]
   
   def index
-    @topics = Topic.all
+    @topics = Topic.visible_to(current_user)
   end
   
   def show
     @topic = Topic.find(params[:id])
+    
+    unless @topic.public || current_user
+      flash[:alert] = "You must be signed in to view private topics."
+      redirect_to new_session_path
+    end
   end
   
   def new
@@ -61,7 +66,7 @@ class TopicsController < ApplicationController
   
   private
   def topic_params
-    params.require(:topic).permit(:name, :description, :pubic)
+    params.require(:topic).permit(:name, :description, :public)
   end
   
   def authorize_user
